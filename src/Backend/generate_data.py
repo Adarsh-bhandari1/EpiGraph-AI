fd202b07a19b13a17fa15cd020ba934160ba4528
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2026-02-10 23:39:40 +0530

    some changes in simulation file and generatedataset file

---
+++ src/Backend/generate_data.py
@@ -0,0 +1,7 @@
+import csv
+import random 
+import networkx as nx
+from models import Node, HealthState
+from simulation import initialize_simulation, run_simulation , add_edges
+
+output_file = "dataset.csv"
--- src/Backend/simulation.py
+++ src/Backend/simulation.py
@@ -23,13 +23,29 @@ def initialize_simulation(num_nodes=100):
 
 def add_edges(k=5):
     # each node connected to k RANDOM neighbors
+    k=min(k , len(nodes)-1)
     for node in nodes:
-        possible_targets = [n.id for n in nodes if n.id != node.id]
-        neighbours = random.sample(possible_targets, k)
+        possible_targets = [n.id for n in nodes if n.id != node.id]  #List of all nodes except the current node
+        neighbours = random.sample(possible_targets, k) #Select  k random neighbors
 
         for target_id in neighbours:
-            g.add_edge(node.id, target_id)
+            g.add_edge(node.id, target_id)  #Add edge Between the current node and the selected neighbors
 
+def run_simulation(steps=10, infection_prob=0.3):
+    for _ in range(steps):
+        new_infected = []
+
+        for node in nodes:
+            if node.state == HealthState.INFECTED:
+                for neighbor_id in g.neighbors(node.id):
+                    neighbor = g.nodes[neighbor_id]["data"]
+
+                    if neighbor.state == HealthState.SUSCEPTIBLE:
+                        if random.random() < infection_prob:
+                            new_infected.append(neighbor)
+
+        for n in new_infected:
+            n.state = HealthState.INFECTED
 
 # run simulation setup
 g, nodes = initialize_simulation()
