bdb8b75cccc00ef9e23b17e09641372ec36ec514
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2026-02-11 19:34:13 +0530

    generating csv dataset for the ML model

--- src/Backend/generate_data.py
+++ src/Backend/generate_data.py
@@ -3,5 +3,16 @@ import random
 import networkx as nx
 from models import Node, HealthState
 from simulation import initialize_simulation, run_simulation , add_edges
+g=nx.Graph()
+nodes=[]
+output_file_path= "dataset.csv"
+def generate_graph():
+    g,nodes = initialize_simulation()
+    add_edges()
+    run_simulation()
+    return g, nodes
 
-output_file = "dataset.csv"
+def save_to_csv(nodes , output_file_path):
+    with open(output_file_path , 'w' , newline='') as csvfile:
+        csv_writer=csv.writer(csvfile)
+        csv_writer.writerow(['Node ID' , 'Health' , 'Positon_X','Postion_Y' , ])
