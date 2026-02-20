a81d8e0a436df634fdb0e83317ec725b5beb8e41
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2026-02-20 23:49:24 +0530

    fix simulation

--- src/Backend/simulation.py
+++ src/Backend/simulation.py
@@ -1,13 +1,10 @@
 import networkx as nx
 import random
-from models import Node, SimulationConfig, HealthState
+from models import Node, HealthState
 
-g = nx.Graph()
-nodes = []
-
-def initialize_simulation(num_nodes=100):
-    g.clear()
-    nodes.clear()
+def initialize_simulation(num_nodes):
+    g = nx.Graph()
+    nodes = []
 
     for i in range(num_nodes):
         n = Node(
@@ -21,24 +18,25 @@ def initialize_simulation(num_nodes=100):
     return g, nodes
 
 
-def add_edges(k=5):
-    # each node connected to k RANDOM neighbors
-    k=min(k , len(nodes)-1)
+def add_edges(graph, nodes, k):
+    k = min(k, len(nodes) - 1)
+
     for node in nodes:
-        possible_targets = [n.id for n in nodes if n.id != node.id]  #List of all nodes except the current node
-        neighbours = random.sample(possible_targets, k) #Select  k random neighbors
+        possible_targets = [n.id for n in nodes if n.id != node.id]
+        neighbors = random.sample(possible_targets, k)
 
-        for target_id in neighbours:
-            g.add_edge(node.id, target_id)  #Add edge Between the current node and the selected neighbors
+        for target_id in neighbors:
+            graph.add_edge(node.id, target_id)
 
-def run_simulation(steps=10, infection_prob=0.3):
+
+def run_simulation(graph, nodes, steps, infection_prob):
     for _ in range(steps):
         new_infected = []
 
         for node in nodes:
             if node.state == HealthState.INFECTED:
-                for neighbor_id in g.neighbors(node.id):
-                    neighbor = g.nodes[neighbor_id]["data"]
+                for neighbor_id in graph.neighbors(node.id):
+                    neighbor = graph.nodes[neighbor_id]["data"]
 
                     if neighbor.state == HealthState.SUSCEPTIBLE:
                         if random.random() < infection_prob:
@@ -47,6 +45,19 @@ def run_simulation(steps=10, infection_prob=0.3):
         for n in new_infected:
             n.state = HealthState.INFECTED
 
-# run simulation setup
-g, nodes = initialize_simulation()
-add_edges()
+
+def run_full_simulation(num_nodes, k, steps, infection_prob, initial_infected):
+    graph, nodes = initialize_simulation(num_nodes)
+    add_edges(graph, nodes, k)
+
+    # Infect initial nodes
+    for n in random.sample(nodes, initial_infected):
+        n.state = HealthState.INFECTED
+
+    run_simulation(graph, nodes, steps, infection_prob)
+
+    infected_after = sum(
+        1 for n in nodes if n.state == HealthState.INFECTED
+    )
+
+    return graph, nodes, infected_after
