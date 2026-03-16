2789c5f6884c44cc10a3a76fb0dd5dc8586de290
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2026-03-16 17:55:47 +0530

    add final model file

--- src/Backend/train_model.py
+++ src/Backend/train_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np 
 from sklearn.model_selection import train_test_split
-from xgboost import  XGBRegressor
+from xgboost import  XGBRegressor 
 from sklearn.metrics import mean_absolute_error , r2_score
 import joblib
 df=pd.read_csv("dataset.csv")
@@ -10,11 +10,16 @@ X  = df.drop("infected_after_10" , axis=1)
 y= df["infected_after_10"]
 X_train , X_test , y_train , y_test = train_test_split(X,y,test_size=0.2 , random_state=42)
 #defining the model
-model= XGBRegressor(n_estimators=300 , learning_rate=0.05 , max_depth=6 , random_state=42)
+model= XGBRegressor(n_estimators=300 , learning_rate=0.05 , max_depth=6 , random_state=42 )
 
 #training the model
 
-model.fit(X_train , y_train , eval_set=[(X_test , y_test)] , early_stopping_rounds=20 , verbose=False)
+model.fit(
+    X_train , 
+    y_train , 
+    eval_set=[(X_test , y_test)]  ,
+    verbose=False
+)
 
 predictions=model.predict(X_test)
 
